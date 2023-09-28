f563c3ccf5fb5e5c194bdbf0b1638a34cb393cc8
Marek Zborowski <marek7654@gmail.com>
2023-09-28 20:49:03 +0200

    Add CountriesRepository

---
+++ backend/src/Repository/CountriesRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Countries;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class CountriesRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Countries::class);
+    }
+
+    public function findAllCountries(): array
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->select('c.countryName as country', 'c.countryId as id')
+            ->getQuery();
+
+        return $qb->getResult();
+    }
+}
