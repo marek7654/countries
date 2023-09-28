f322504dd1508203532e9dbf6ef5f19975d87feb
Marek Zborowski <marek7654@gmail.com>
2023-09-28 22:46:54 +0200

    Add api-connector

---
+++ frontend/src/helpers/api-connector.js
@@ -0,0 +1,21 @@
+const ENDPOINT = 'http://localhost:8000/api/countries';
+
+export const fetchData = async (id = false) => {
+  const url = id ? `${ENDPOINT}/${id}` : ENDPOINT;
+  
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        `Nieudane zapytanie do API. Kod statusu: ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+    console.log('data', data);
+    return data;
+  } catch (error) {
+    console.error('Wystąpił błąd podczas pobierania danych z API:', error);
+  }
+};
